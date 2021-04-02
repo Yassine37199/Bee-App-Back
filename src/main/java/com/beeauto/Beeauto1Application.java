64638a0eacf085066db1751753688806150db7a6
unknown <achrafsalmi08@gmail.com>
2021-04-02 16:15:42 +0100

    creation projet

---
+++ src/main/java/com/beeauto/Beeauto1Application.java
@@ -0,0 +1,13 @@
+package com.beeauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beeauto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beeauto1Application.class, args);
+	}
+
+}
