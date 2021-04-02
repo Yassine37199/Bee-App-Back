54b87935ec540698b7db0c42aff8261003d967f9
TaherMz <taher.mzid@gmail.com>
2021-04-02 19:14:54 +0100

    client controller

--- src/main/java/com/beeauto/entites/Client.java
+++ src/main/java/com/beeauto/entites/Client.java
@@ -10,8 +10,6 @@ import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Entity
 public class Client {
--- src/main/java/com/beeauto/repositories/ClientRepository.java
+++ src/main/java/com/beeauto/repositories/ClientRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.beeauto.entites.Client;
 
-public interface clientRepository extends CrudRepository<Client, Long>{
+public interface ClientRepository extends CrudRepository<Client, Long>{
 
 }
