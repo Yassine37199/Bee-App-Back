e30f3a3a7b077925d1be97c51348da7b790008ce
TaherMz <taher.mzid@gmail.com>
2021-04-02 18:38:39 +0100

    client repo

---
+++ src/main/java/com/beeauto/repositories/clientRepository.java
@@ -0,0 +1,9 @@
+package com.beeauto.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.beeauto.entites.Client;
+
+public interface clientRepository extends CrudRepository<Client, Long>{
+
+}
