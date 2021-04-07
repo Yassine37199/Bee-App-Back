7fe4aa0834b4237a1df4734594e483df75a19838
Yosr <yaya.ayadi1919@gmail.com>
2021-04-07 15:08:09 +0200

    update user

--- src/main/java/com/beeauto/Controllers/UserController.java
+++ src/main/java/com/beeauto/Controllers/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.beeauto.Entities.Role;
 import com.beeauto.Entities.User;
 import com.beeauto.Exception.ResourceNotFoundException;
 import com.beeauto.Repository.RoleRepository;
@@ -41,16 +42,21 @@ public class UserController {
 		
 	}
 	
-	@PutMapping("/{userId}")
-	public User updateUser(@PathVariable Long userId, @Valid @RequestBody User userRequest) {
+	@PutMapping("update/{userId}/{roleId}")
+	public User updateUser(@PathVariable(value = "userId") Long userId,
+			               @PathVariable(value = "roleId") Long roleId,
+			@Valid @RequestBody User userRequest ) {
 		return 
 				userRepository.findById(userId).map(user -> {
+					Role role = roleRepository.findById(roleId)
+							.orElseThrow(() -> new ResourceNotFoundException("errreeeur"));
 					user.setNom(userRequest.getNom());
 					user.setPrenom(userRequest.getPrenom());
 					user.setPassword(userRequest.getPassword());
 					user.setTel(userRequest.getTel());
 					user.setId_role(userRequest.getId_role());
 					user.setActive(userRequest.getActive());
+					user.setRole(role);
 					return userRepository.save(user);
 					}).orElseThrow(() -> new
 							ResourceNotFoundException("UserId" + userId + "not found"));
