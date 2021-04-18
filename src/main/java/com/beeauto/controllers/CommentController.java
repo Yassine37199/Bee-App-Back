6e1e44253d1974aa258f991c446b4e9462a8f00c
Yassine37199 <yassine.ouri7319@gmail.com>
2021-04-18 22:23:10 +0100

    Fixed and tested addCommentaire function

--- src/main/java/com/beeauto/controllers/CommentController.java
+++ src/main/java/com/beeauto/controllers/CommentController.java
@@ -2,9 +2,12 @@ package com.beeauto.controllers;
 
 import com.beeauto.entities.Commentaire;
 import com.beeauto.entities.Ticket;
+import com.beeauto.entities.User;
 import com.beeauto.exceptions.ResourceNotFoundException;
 import com.beeauto.repositories.CommentRepository;
 import com.beeauto.repositories.TicketRepository;
+import com.beeauto.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,10 +20,13 @@ public class CommentController {
 
     private final CommentRepository commentRepository;
     private final TicketRepository ticketRepository;
+    private final UserRepository userRepository;
 
-    public CommentController(CommentRepository commentRepository, TicketRepository ticketRepository) {
+    @Autowired
+    public CommentController(CommentRepository commentRepository, TicketRepository ticketRepository, UserRepository userRepository) {
         this.commentRepository = commentRepository;
         this.ticketRepository = ticketRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/get/all")
@@ -38,12 +44,16 @@ public class CommentController {
 
     }
 
-    @PostMapping("/add/{ticketId}")
-    public ResponseEntity<Commentaire> addComment(@PathVariable("ticketId") Long id,
+    @PostMapping("/add/{ticketId}/{idUser}")
+    public ResponseEntity<Commentaire> addComment(@PathVariable("ticketId") Long ticketId,
+                                                  @PathVariable("idUser") Long idUser,
                                                   @RequestBody Commentaire comment) {
-        Ticket ticket = ticketRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Ticket By id " + id + " does not exist"));
+        Ticket ticket = ticketRepository.findById(ticketId)
+                .orElseThrow(() -> new ResourceNotFoundException("Ticket By id " + ticketId + " does not exist"));
+        User user = userRepository.findById(idUser)
+                .orElseThrow(() -> new ResourceNotFoundException("User By id " + idUser + " does not exist"));
         comment.setTicket(ticket);
+        comment.setUser(user);
         commentRepository.save(comment);
         return new ResponseEntity<>(comment, HttpStatus.CREATED);
     }
