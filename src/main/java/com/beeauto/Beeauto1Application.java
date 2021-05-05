568f8e02572efa98d9a3c041a45ad4034d9bf995
Yassine37199 <yassine.ouri7319@gmail.com>
2021-05-05 21:17:35 +0100

    Config Modem/Abonnement Relation fix

--- src/main/java/com/beeauto/Beeauto1Application.java
+++ src/main/java/com/beeauto/Beeauto1Application.java
@@ -3,8 +3,6 @@ package com.beeauto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@EnableAutoConfiguration
 @SpringBootApplication
 public class Beeauto1Application {
 
